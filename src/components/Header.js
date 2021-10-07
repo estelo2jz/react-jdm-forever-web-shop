0381c9ae739ff597eb1c788147d3638920e38c05
esteloabell <manitobaaa@gmail.com>
2021-10-07 15:03:42 -0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,18 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext } from 'react';
+import * as ReactBootStrap from "react-bootstrap";
+import {
+  BrowserRouter as Router,
+  Link
+} from "react-router-dom";
 import Menu from './svg/bars-solid.svg'
 import Close from './svg/times-solid.svg'
 import Cart from './svg/cart.svg'
-import { Link } from 'react-router-dom'
 import { DataContext } from './DataProvider'
 
-import JDMLOGO from '../assets/images/home/jdmlover.png'
+import JDMLOGO from '../assets/images/home/jdmlover3.png'
 
 import { GiCarSeat } from 'react-icons/gi';
+import { BiMenuAltRight } from 'react-icons/bi';
 
 import './Header/Header.scss';
 
@@ -26,45 +31,61 @@ export default function Header() {
 
   return (
     <header>
-      <div className="menu" onClick={toggleMenu}>
-        <img src={Menu} alt="" width="30" />
-      </div>
-
-      <div className="logo">
-        {/* <h1><Link to="/products">JDM-Eien-Ni</Link></h1> */}
-        <Link className="logo__link" to="/">
-          <img src={JDMLOGO} />
-          {/* <p>
-            JDM 
-          </p>
-          <p>
-            <GiCarSeat /> 
-          </p>
-          <p>
-            OVER
-          </p> */}
-        </Link>
-      </div>
-      <ul style={styleMenu}>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/products">Inventory</Link></li>
-        {/* <li><Link to="/">FIND YOUR VEHICLE</Link></li> */}
-        <li><Link to="/">SHOP</Link></li>
-        {/* <li><Link to="/">NEWS</Link></li> */}
-        {/* <li><Link to="/">CALIFORNIA BUYER</Link></li> */}
-        {/* <li><Link to="/">FAQ | NFO</Link></li> */}
-        <li><Link to="/">Contact</Link></li>
-        {/* <li><Link to="/">Login / Register</Link></li> */}
-        <li onClick={toggleMenu}>
-          <img src={Close} alt="" width="30" className="menu" />
-        </li>
-      </ul>
-      {/* <div className="cart-icon">
-        <span>{cart.length}</span>
-        <Link to="/cart">
-          <img src={Cart} alt="" width="30" />
-        </Link>
-      </div> */}
+      <ReactBootStrap.Navbar collapseOnSelect expand="xl" bg="light" variant="light" className="header__container">
+        <ReactBootStrap.Navbar.Brand className="header__container" href="#home">
+          <Link className="logo__link" to="/">
+            {/* <img src={JDMLOGO} alt="" /> */}
+            <span>
+              JDM
+            </span>
+            <span>
+              <GiCarSeat />
+            </span>
+            <span>
+              olver
+            </span>
+          </Link>
+        </ReactBootStrap.Navbar.Brand>
+        <ReactBootStrap.Navbar.Toggle className="header__toggle" aria-controls="responsive-navbar-nav" />
+        <ReactBootStrap.Navbar.Collapse className="header__collapse" id="responsive-navbar-nav">
+          <ReactBootStrap.Nav className="mr-auto">
+            <Link className="header__link" to="/">
+              <ReactBootStrap.Nav.Link className="header__links" href="#features">Home</ReactBootStrap.Nav.Link>
+            </Link>
+            <Link className="header__link" to="/products/">
+              <ReactBootStrap.Nav.Link className="header__links" href="#pricing">Inventory</ReactBootStrap.Nav.Link>
+            </Link>
+            <ReactBootStrap.NavDropdown className="header__dropdown" title="SHOP" id="collasible-nav-dropdown">
+              <ReactBootStrap.NavDropdown.Item className="header__dropdown-links" href="#action/3.1">CURRENT INVENTORY</ReactBootStrap.NavDropdown.Item>
+              <ReactBootStrap.NavDropdown.Item className="header__dropdown-links" href="#action/3.2">SOLD INVENTORY</ReactBootStrap.NavDropdown.Item>
+              <ReactBootStrap.NavDropdown.Item className="header__dropdown-links" href="#action/3.3">FUTURE INVENTORY</ReactBootStrap.NavDropdown.Item>
+              <ReactBootStrap.NavDropdown.Item className="header__dropdown-links" href="#action/3.3">JAPAN INVENTORY</ReactBootStrap.NavDropdown.Item>
+              {/* <ReactBootStrap.NavDropdown.Divider /> */}
+              {/* <ReactBootStrap.NavDropdown.Item href="#action/3.4">Separated link</ReactBootStrap.NavDropdown.Item> */}
+            </ReactBootStrap.NavDropdown>
+          </ReactBootStrap.Nav>
+          <ReactBootStrap.Nav className="header__bottom">
+            <Link className="header__bottom-link" to="/">
+              <ReactBootStrap.Nav.Link className="header__bottom-links" eventKey={2} href="#memes">
+                NEWS
+              </ReactBootStrap.Nav.Link>
+            </Link>
+            <Link className="header__bottom-link" to="/">
+              <ReactBootStrap.Nav.Link className="header__bottom-links" eventKey={2} href="#memes">
+                CALIFORNIA BUYER
+              </ReactBootStrap.Nav.Link>
+            </Link>
+            <Link className="header__bottom-link" to="/">
+              <ReactBootStrap.Nav.Link className="header__bottom-links" href="#deets">FAQ | INFO</ReactBootStrap.Nav.Link>
+            </Link>
+            <Link className="header__bottom-link" to="/">
+              <ReactBootStrap.Nav.Link className="header__bottom-links" eventKey={2} href="#memes">
+                CONTACT
+              </ReactBootStrap.Nav.Link>
+            </Link>
+          </ReactBootStrap.Nav>
+        </ReactBootStrap.Navbar.Collapse>
+      </ReactBootStrap.Navbar>
     </header>
   )
 }
