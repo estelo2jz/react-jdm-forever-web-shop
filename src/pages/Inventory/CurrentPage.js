aada48e7b8871daf1112002ca1438ec2c96b70e8
esteloabell <manitobaaa@gmail.com>
2021-10-08 17:31:26 -0500

    Update CurrentPage.js

--- src/pages/Inventory/CurrentPage.js
+++ src/pages/Inventory/CurrentPage.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Products from '../../components/Products';
 import Current from '../../components/Product/Current';
+import HeaderInventory from '../../components/Home/HeaderInventory';
 
 const CurrentPage = () => {
   return (
     <div>
       <Current heading="CURRENT INVENTORY" />
+      <HeaderInventory />
+
       <Products />
     </div>
   )
