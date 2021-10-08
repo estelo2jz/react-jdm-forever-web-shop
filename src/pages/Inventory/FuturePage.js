1055288e72f60a4928ae1c1c9bfaa5818e3ba7ae
esteloabell <manitobaaa@gmail.com>
2021-10-08 17:31:29 -0500

    Update FuturePage.js

--- src/pages/Inventory/FuturePage.js
+++ src/pages/Inventory/FuturePage.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Products from '../../components/Products';
 import Current from '../../components/Product/Current';
+import HeaderInventory from '../../components/Home/HeaderInventory';
 
 const FuturePage = () => {
   return (
     <div>
       <Current heading="FUTURE INVENTORY" />
       <Products />
+      <HeaderInventory />
+
     </div>
   )
 }
