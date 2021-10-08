494870218add7b12c654627ab3e0fae7d740ce4e
esteloabell <manitobaaa@gmail.com>
2021-10-07 20:17:22 -0500

    Update AboutThumb.js

--- src/components/Details/AboutThumb.js
+++ src/components/Details/AboutThumb.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-import './Thumb.scss';
+import './AboutThumb.scss';
 
 export default function AboutThumb({ aboutUs, setIndex }) {
   return (
-    <div className="thumb">
+    <div className="about-thumb">
       {
         aboutUs.map((item, index) => (
           <p>{item}</p>
