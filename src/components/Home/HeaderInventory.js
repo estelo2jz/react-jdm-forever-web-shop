0bf57a437192ec176c63f72932295d95750b722f
esteloabell <manitobaaa@gmail.com>
2021-10-08 12:53:40 -0500

    Create HeaderInventory.js

---
+++ src/components/Home/HeaderInventory.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './HeaderInventory.scss';
+
+const HeaderInventory = () => {
+  return (
+    <div>
+      <div className="top-header__link-container">
+        <Link className="top-header__link" to="/products">
+          <p className="top-header__link-main">VIEW ALL INVENTORY</p>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default HeaderInventory
