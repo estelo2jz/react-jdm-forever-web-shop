500def2c04ed7cc9d50c5b993ed34ab19fbd72c7
esteloabell <manitobaaa@gmail.com>
2021-10-08 16:06:54 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/Header'
 import Products from './components/Products'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import  Component, { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { DataProvider } from './components/DataProvider'
 import Details from './components/Details'
 import Home from './components/Home/Home';
@@ -12,8 +12,13 @@ import ScrollToTop from './components/ScrollToTop'
 
 import Contact from '../src/pages/Contact/Contact';
 import FindVehicle from '../src/pages/FindVehicle/FindVehicle';
-import Inventory from '../src/pages/Inventory/Inventory';
 import News from '../src/pages/News/News';
+
+import Inventory from '../src/pages/Inventory/Inventory';
+import CurrentPage from '../src/pages/Inventory/CurrentPage';
+import SoldPage from '../src/pages/Inventory/SoldPage';
+import FuturePage from '../src/pages/Inventory/FuturePage';
+import JapanPage from '../src/pages/Inventory/JapanPage';
 
 
 function App() {
@@ -25,10 +30,14 @@ function App() {
             <Header />
             <section>
               <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="inventory" element={<Inventory />} />
-                <Route path="inventory/:id" element={<Details />} />
-                <Route path="cart" element={<Cart />} />
+                <Route path="/" exact element={<Home />} />
+                <Route path="/inventory" exact element={<Inventory />} />
+                <Route path="/inventory/current-inventory" exact element={<CurrentPage />} />
+                <Route path="/inventory/sold" exact element={<SoldPage />} />
+                <Route path="/inventory/future-inventory" exact element={<FuturePage />} />
+                <Route path="/inventory/japan-inventory" exact element={<JapanPage />} />
+                <Route path="/inventory/:id" exact element={<Details />} />
+                <Route path="/cart" exact element={<Cart />} />
               </Routes>
             </section>
             <Footer />
