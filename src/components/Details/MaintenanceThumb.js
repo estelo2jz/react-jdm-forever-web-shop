3e33748f3b2ff6c03f57860d2e557b047d27fd4f
esteloabell <manitobaaa@gmail.com>
2021-10-08 11:33:27 -0500

    Update MaintenanceThumb.js

--- src/components/Details/MaintenanceThumb.js
+++ src/components/Details/MaintenanceThumb.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
+
+import './Maintenance.scss';
 
 export default function MaintenanceThumb({ maintenanceService, setIndex }) {
   return (
-    <div className="thumb">
+    <div className="maintenance">
       {
         maintenanceService.map((item, index) => (
-          <p key={index}>{item}</p>
+          <div className="maintenance__container">
+            <p key={index}>{item}</p>
+          </div>
         ))
       }
     </div>
