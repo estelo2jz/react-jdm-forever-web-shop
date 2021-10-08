2386d1dc6ad6de67203e2bcbfc40eddd960c3303
esteloabell <manitobaaa@gmail.com>
2021-10-08 17:31:31 -0500

    Update Inventory.js

--- src/pages/Inventory/Inventory.js
+++ src/pages/Inventory/Inventory.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Products from '../../components/Products';
 import Current from '../../components/Product/Current';
+import HeaderInventory from '../../components/Home/HeaderInventory';
 
 const Inventory = () => {
   return (
     <div>
       <Current heading="CURRENT INVENTORY" />
       <Products />
+      <HeaderInventory />
     </div>
   )
 }
