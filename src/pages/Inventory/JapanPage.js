52087234c1f90e370203f9bd97f5a2dd4ddfbb51
esteloabell <manitobaaa@gmail.com>
2021-10-08 17:31:34 -0500

    Update JapanPage.js

--- src/pages/Inventory/JapanPage.js
+++ src/pages/Inventory/JapanPage.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Products from '../../components/Products';
 import Current from '../../components/Product/Current';
+import HeaderInventory from '../../components/Home/HeaderInventory';
 
 const JapanPage = () => {
   return (
     <div>
       <Current heading="JAPAN INVENTORY"  />
       <Products />
+      <HeaderInventory />
+
     </div>
   )
 }
