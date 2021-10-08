6701f3ad3005cf2849aa52a507cc6b92ecec91c6
esteloabell <manitobaaa@gmail.com>
2021-10-08 17:31:36 -0500

    Update SoldPage.js

--- src/pages/Inventory/SoldPage.js
+++ src/pages/Inventory/SoldPage.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Products from '../../components/Products';
 import Current from '../../components/Product/Current';
+import HeaderInventory from '../../components/Home/HeaderInventory';
 
 const SoldPage = () => {
   return (
     <div>
       <Current heading="RECENTLY SOLD" />
       <Products />
+      <HeaderInventory />
+
     </div>
   )
 }
