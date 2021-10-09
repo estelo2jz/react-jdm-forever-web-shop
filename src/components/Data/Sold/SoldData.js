3243b02f9746f69b98d80f8dd38aeef5637d7dec
esteloabell <manitobaaa@gmail.com>
2021-10-09 00:35:19 -0500

    Create SoldData.js

---
+++ src/components/Data/Sold/SoldData.js
@@ -0,0 +1,48 @@
+import React, { useContext } from 'react'
+import { SoldDataContext } from './SoldInventory';
+import { Link } from 'react-router-dom'
+
+import './styles/SoldData.scss';
+
+export default function CurrentData() {
+  const value = useContext(SoldDataContext)
+  const [products] = value.products
+  const addCart = value.addCart
+
+  return (
+    <div>
+      {/* <Current /> */}
+      <div className="products">
+        {
+          products.map(product => (
+            <div className="card" key={product._id}>
+              <Link to={`/inventory/${product._id}`}>
+                <img src={product.images[0]} alt="" />
+              </Link>
+              <div className="box-container">
+                <div className="box-product">
+                  <div className="box-product-name">
+                    <h3 className="box-product-year" year={product.year}>
+                      <Link to={`/inventory/${product._id}`} className="box-product-year-name">{product.year}</Link>
+                    </h3>
+                    <h3 className="box-product-make" make={product.make}>
+                      <Link to={`/inventory/${product._id}`} className="box-product-make-name">{product.make}</Link>
+                    </h3>
+                  </div>
+                  <h3 className="box-product-model" model={product.model}>
+                    <Link to={`/inventory/${product._id}`} className="box-product-model-name">{product.model}</Link>
+                  </h3>
+                </div>
+                <div className="box-price">
+                  <p>OFFERED AT:</p>
+                  <h4>${product.price}</h4>
+                </div>
+              </div>
+            </div>
+          ))
+        }
+      </div>
+
+    </div>
+  )
+}
