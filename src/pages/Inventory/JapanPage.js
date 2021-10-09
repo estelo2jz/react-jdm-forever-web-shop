8d4f0d4ca3aa9f7f2d8b5520814dc3cb33e115ea
esteloabell <manitobaaa@gmail.com>
2021-10-09 00:35:57 -0500

    Update JapanPage.js

--- src/pages/Inventory/JapanPage.js
+++ src/pages/Inventory/JapanPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Products from '../../components/Products';
 import Current from '../../components/Product/Current';
 import HeaderInventory from '../../components/Home/HeaderInventory';
+import JapanData from '../../components/Data/Japan/JapanData';
 
 const JapanPage = () => {
   return (
