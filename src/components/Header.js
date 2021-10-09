1304392668ba53ea8d2d7916bc7ea295f4074015
esteloabell <manitobaaa@gmail.com>
2021-10-09 00:35:00 -0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,10 @@ import {
 import Menu from './svg/bars-solid.svg'
 import Close from './svg/times-solid.svg'
 import Cart from './svg/cart.svg'
+import { CurrentDataProvider } from './Data/Current/CurrentInventory';
+import { SoldDataProvider } from './Data/Sold/SoldInventory';
+import { FutureDataProvider } from './Data/Future/FutureInventory';
+import { JapanDataProvider } from './Data/Japan/JapanInventory';
 import { DataContext } from './DataProvider'
 
 import JDMLOGO from '../assets/images/home/jdmlover3.png'
@@ -18,7 +22,7 @@ import './Header/Header.scss';
 
 export default function Header() {
   const [menu, setMenu] = useState(false)
-  const value = useContext(DataContext)
+  const value = useContext(DataContext, CurrentDataProvider, SoldDataProvider, FutureDataProvider, JapanDataProvider)
   const [cart] = value.cart
 
   const toggleMenu = () => {
@@ -52,10 +56,7 @@ export default function Header() {
             <Link className="header__link" to="/">
               <ReactBootStrap.Nav.Link className="header__links" href="#features">Home</ReactBootStrap.Nav.Link>
             </Link>
-            <Link className="header__link" to="/inventory/">
-              <ReactBootStrap.Nav.Link className="header__links" href="#pricing">Inventory</ReactBootStrap.Nav.Link>
-            </Link>
-            <ReactBootStrap.NavDropdown className="header__dropdown" title="SHOP" id="collasible-nav-dropdown">
+            <ReactBootStrap.NavDropdown className="header__dropdown" title="Inventory" id="collasible-nav-dropdown">
               <ReactBootStrap.NavDropdown.Item className="header__dropdown-links" >
                 <Link className="header__dropdown-links" to="/inventory/">
                   CURRENT INVENTORY
@@ -79,6 +80,12 @@ export default function Header() {
               {/* <ReactBootStrap.NavDropdown.Divider /> */}
               {/* <ReactBootStrap.NavDropdown.Item href="#action/3.4">Separated link</ReactBootStrap.NavDropdown.Item> */}
             </ReactBootStrap.NavDropdown>
+            <Link className="header__link" to="/inventory/">
+              <ReactBootStrap.Nav.Link className="header__links" href="#pricing">Find Your Vehicle</ReactBootStrap.Nav.Link>
+            </Link>
+            <Link className="header__link" to="/inventory/">
+              <ReactBootStrap.Nav.Link className="header__links" href="#pricing">Shop</ReactBootStrap.Nav.Link>
+            </Link>
           </ReactBootStrap.Nav>
           <ReactBootStrap.Nav className="header__bottom">
             <Link className="header__bottom-link" to="/">
