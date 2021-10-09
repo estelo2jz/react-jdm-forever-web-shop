24b8dd3fee250363c9780c614660049a3d8db3c3
esteloabell <manitobaaa@gmail.com>
2021-10-09 00:35:52 -0500

    Update FuturePage.js

--- src/pages/Inventory/FuturePage.js
+++ src/pages/Inventory/FuturePage.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Products from '../../components/Products';
 import Current from '../../components/Product/Current';
 import HeaderInventory from '../../components/Home/HeaderInventory';
+import FutureData from '../../components/Data/Future/FutureData';
+
 
 const FuturePage = () => {
   return (
