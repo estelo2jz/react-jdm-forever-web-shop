5f255f287657c2e73d6b6b708e1a0fd53c30ae20
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-10-11 12:17:47 -0500

    Update SoldData.js

--- src/components/Data/Sold/SoldData.js
+++ src/components/Data/Sold/SoldData.js
@@ -12,30 +12,30 @@ export default function CurrentData() {
   return (
     <div>
       {/* <Current /> */}
-      <div className="products">
+      <div className="sold-data__products">
         {
           products.map(product => (
-            <div className="card" key={product._id}>
+            <div className="sold-data__card" key={product._id}>
               <Link to={`/inventory/${product._id}`}>
+                <div className="sold-data__box-price">
+                  <h4>{product.price}</h4>
+                  {/* <h6>{product.coming}</h6> */}
+                </div>
                 <img src={product.images[0]} alt="" />
               </Link>
-              <div className="box-container">
-                <div className="box-product">
-                  <div className="box-product-name">
-                    <h3 className="box-product-year" year={product.year}>
-                      <Link to={`/inventory/${product._id}`} className="box-product-year-name">{product.year}</Link>
+              <div className="sold-data__box-container">
+                <div className="sold-data__box-product">
+                  <div className="sold-data__box-product-name">
+                    <h3 className="sold-data__box-product-year" year={product.year}>
+                      <Link to={`/inventory/${product._id}`} className="sold-data__box-product-year-name">{product.year}</Link>
                     </h3>
-                    <h3 className="box-product-make" make={product.make}>
-                      <Link to={`/inventory/${product._id}`} className="box-product-make-name">{product.make}</Link>
+                    <h3 className="sold-data__box-product-make" make={product.make}>
+                      <Link to={`/inventory/${product._id}`} className="sold-data__box-product-make-name">{product.make}</Link>
                     </h3>
                   </div>
-                  <h3 className="box-product-model" model={product.model}>
-                    <Link to={`/inventory/${product._id}`} className="box-product-model-name">{product.model}</Link>
+                  <h3 className="sold-data__box-product-model" model={product.model}>
+                    <Link to={`/inventory/${product._id}`} className="sold-data__box-product-model-name">{product.model}</Link>
                   </h3>
-                </div>
-                <div className="box-price">
-                  <p>OFFERED AT:</p>
-                  <h4>${product.price}</h4>
                 </div>
               </div>
             </div>
