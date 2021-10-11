4060a711fa3e993b5e39a02a2c99eb947b400183
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-10-11 14:42:30 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ import { FutureDataProvider} from './components/Data/Future/FutureInventory';
 import { JapanDataProvider } from './components/Data/Japan/JapanInventory';
 import  { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { DataProvider } from './components/DataProvider'
+import ScrollToTop from './components/ScrollToTop';
+
 import Details from './components/Details'
 import Home from './components/Home/Home';
 import Cart from './components/Cart'
 import Footer from './components/Footer/Footer';
-import ScrollToTop from './components/ScrollToTop';
 import Contact from '../src/pages/Contact/Contact';
 import FindVehicle from '../src/pages/FindVehicle/FindVehicle';
 import News from '../src/pages/News/News';
@@ -19,8 +20,8 @@ import News from '../src/pages/News/News';
 import Inventory from '../src/pages/Inventory/Inventory';
 import CurrentPage from '../src/pages/Inventory/CurrentPage';
 import SoldPage from '../src/pages/Inventory/SoldPage';
-import FuturePage from '../src/pages/Inventory/FuturePage';
 import JapanPage from '../src/pages/Inventory/JapanPage';
+import FuturePage from '../src/pages/Inventory/FuturePage';
 
 
 function App() {
@@ -43,6 +44,10 @@ function App() {
                           <Route path="/inventory/sold" exact element={<SoldPage />} />
                           <Route path="/inventory/future-inventory" exact element={<FuturePage />} />
                           <Route path="/inventory/japan-inventory" exact element={<JapanPage />} />
+
+                          <Route path="/car-finder" exact element={<FindVehicle />} />
+                          <Route path="/media" exact element={<News />} />
+                          <Route path="/contact" exact element={<Contact />} />
                           <Route path="/inventory/:id" exact element={<Details />} />
                           <Route path="/cart" exact element={<Cart />} />
                         </Routes>
