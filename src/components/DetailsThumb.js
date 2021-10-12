19f51f9708b83cf31e87af2f3e7c3f04620c0862
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-10-12 12:35:18 -0500

    Update DetailsThumb.js

--- src/components/DetailsThumb.js
+++ src/components/DetailsThumb.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from 'react';
+// import './DetailsThumb.scss';
 
 export default function DetailsThumb({ images, setIndex }) {
   return (
     <div className="thumb">
       {
         images.map((img, index) => (
+        <div>
+
           <img src={img} alt="" key={index}
             onClick={() => setIndex(index)} />
+        </div>
         ))
       }
     </div>
